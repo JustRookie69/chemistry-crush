d2f3ce70a0cf0dbf3bb42c36a74d575c392f0615
Shivansh <shivanshy6@gmail.com>
2025-03-12 12:24:20 +0530

    Add chemistry crush project files

---
+++ chemistry-crush/src/App.js
@@ -0,0 +1,13 @@
+// App.js
+import React from 'react';
+import ChemistryCrush from './components/ChemistryCrush';
+
+function App() {
+  return (
+    <div className="App">
+      <ChemistryCrush />
+    </div>
+  );
+}
+
+export default App;
